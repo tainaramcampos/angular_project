7930fadd9a4a06138bbf26cd677cfe2bc3058bef
tainaramcampos <tainaramcampos@gmail.com>
2019-11-05 06:42:18 -0300

    Criação do projeto Angular

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+    $scope.foto = {
+        titulo: 'Beach',
+        url: 'https://www.cityofkeywest-fl.gov/egov/images/1562686823_73401_o.jpg'
+    };
+});
